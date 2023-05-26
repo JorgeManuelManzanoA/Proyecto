0afc2b85dcb17edb8ce651a5711b574157c9d031
JorgeManuelManzanoA <128429222+JorgeManuelManzanoA@users.noreply.github.com>
2023-05-26 16:56:19 -0500

    Update conexion.php

--- model/conexion.php
+++ model/conexion.php
@@ -1,11 +1,11 @@
 <?php
-$contrasena = "";
-$usuario = "root";
+$contrasena = "AVNS_Ln5yI4lx_R8UW29lb4s";
+$usuario = "doadmin";
 $nombre_bd = "hospital";
 
 try {
     $bd = new PDO(
-        'mysql:host=localhost;dbname=' . $nombre_bd,
+        'mysql:host=db-mysql-nyc1-61112-do-user-14089120-0.b.db.ondigitalocean.com;dbname=' . $nombre_bd,
         $usuario,
         $contrasena,
         array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
